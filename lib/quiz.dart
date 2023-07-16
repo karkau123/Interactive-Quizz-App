35c5f2b0fed90395ea5cab75dadb7106db3fdef5
Kartikeya Singh <singhkartikeya200@gmail.com>
2023-07-16 14:51:16 +0530

    Quiz Code updated

---
+++ lib/quiz.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizzState();
+  }
+}
+
+class _QuizzState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 33, 27, 93),
+              Color.fromARGB(255, 153, 149, 203),
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          )),
+          child:  activeScreen,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(122, 237, 229, 228),
+          ),
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter The fun Way !',
+            style: TextStyle(
+              color: Color.fromARGB(255, 216, 222, 237),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {
+              // Start Screen Should Be Changed
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Color.fromARGB(247, 250, 239, 239),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quizz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
