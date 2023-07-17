21aa9c378d67c1d458a7d48ebcd7cfd843d61b8f
Kartikeya Singh <singhkartikeya200@gmail.com>
2023-07-17 21:28:47 +0530

    question_screen code competed

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
-class QuestionsScreen extends StatefulWidget{
-  const QuestionsScreen ({super.key});
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreen();
   }
 }
-class _QuestionsScreen extends State<QuestionsScreen>{
+
+class _QuestionsScreen extends State<QuestionsScreen> {
   @override
-  Widget build (context){
-    return const Text ('QuestionsScreen');
+  Widget build(context) {
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              )),
+          const SizedBox(height: 30),
+   
+
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+         // const SizedBox(height: 10),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+        ],
+      ),
+    );
   }
 }
